778a1f4e46d8a8316b73d1058d2eeb538f15358f
H4rmey <hammiekaaz@gmail.com>
2020-07-11 14:01:24 +0200

    Update - Hol up this aint rite

---
+++ Assets/Scripts/Tasks/BaseTask.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseTask
+{
+    public Vector2      origin;
+    public Vector2      destination;
+    public float        time;              //time to complete the task
+    public bool         completedOverride;   //override for the completion of the task
+}
