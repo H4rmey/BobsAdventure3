365b04442fd3928caa9c0c4468804cbc08338bed
djjansse2 <43000159djjansse2@users.noreply.github.com>
2020-07-11 22:14:44 +0200

    Update - Working on first Level

---
+++ Assets/Scripts/Prop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prop : Interactable
+{
+	private void Start()
+	{
+		
+	}
+
+	public override void Interact()
+	{
+		
+	}
+}
