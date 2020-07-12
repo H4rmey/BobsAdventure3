2ace40df4b24e3b222de4d9f09a7671dd37fa138
H4rmey <hammiekaaz@gmail.com>
2020-07-12 19:02:40 +0200

    Added Level2

---
+++ Assets/Scripts/RotateSlightly.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSlightly : MonoBehaviour
+{
+    public float rotationTime = 0.5f;
+    public int rotationAmount = 1;
+
+    private IEnumerator RotateSlightlyFunc()
+    {
+        float currentRotationZ = gameObject.transform.rotation.eulerAngles.z;
+
+        while (true)
+        {
+            rotationAmount *= -1;
+            transform.rotation = Quaternion.Euler(0, 0, currentRotationZ + rotationAmount);
+
+            yield return new WaitForSeconds(rotationTime);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("RotateSlightlyFunc");
+    }
+}
