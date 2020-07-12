323b45dc8278cd300378ab93d4b3c6419f2669f0
H4rmey <hammiekaaz@gmail.com>
2020-07-12 14:45:24 +0200

    Update - added video and scene

---
+++ Assets/Scripts/VideoHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoHandler : MonoBehaviour
+{
+    private VideoPlayer vp;
+    public float videoDuration;
+    public float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > videoDuration)
+            SceneManager.LoadScene("MainMenu");
+    }
+}
